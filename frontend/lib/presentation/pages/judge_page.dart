96991835ba24140893074420bad747865c9e02c0
konchanksu <nyankomi59@gmail.com>
2022-03-26 17:12:48 +0900

    ジャンル選択機能の追加

--- frontend/lib/presentation/pages/judge_page.dart
+++ frontend/lib/presentation/pages/judge_page.dart
@@ -3,29 +3,10 @@ import 'package:frontend/ProductGenre.dart';
 import 'package:frontend/presentation/pages/question_page.dart';
 
 class JudgePage extends StatelessWidget {
-  const JudgePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('購入する商品のジャンルを選択'),
-              JudgePageWidget(),
-              RaisedButton(
-                child: Text('次へ'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => QuestionPage()),
-                  );
-                },
-              ),
-            ],
-          ),
-      ),
+      body: JudgePageWidget(),
     );
   }
 }
@@ -38,36 +19,51 @@ class JudgePageWidget extends StatefulWidget {
   State<JudgePageWidget> createState() => _JudgePageWidgetState();
 }
 
-
 /// This is the private State class that goes with JudgePageWidget.
 class _JudgePageWidgetState extends State<JudgePageWidget> {
   String dropdownValue = '家具・インテリア';
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      icon: const Icon(Icons.arrow_downward),
-      iconSize: 24,
-      elevation: 24,
-      style: const TextStyle(color: Colors.deepPurple),
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('購入する商品のジャンルを選択'),
+          DropdownButton<String>(
+            value: dropdownValue,
+            icon: const Icon(Icons.arrow_downward),
+            iconSize: 24,
+            elevation: 24,
+            style: const TextStyle(color: Colors.deepPurple),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+              });
+            },
+            items: <String>['家具・インテリア', '服', '家電', '美容・コスメ', '食品', 'その他']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+          RaisedButton(
+            child: Text('次へ'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => QuestionPage(this.dropdownValue)),
+              );
+            },
+          ),
+        ],
       ),
-      onChanged: (String? newValue) {
-        setState(() {
-          dropdownValue = newValue!;
-        });
-      },
-      items: <String>['家具・インテリア', '服', '家電', '美容・コスメ', '食品', 'その他']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
     );
   }
 }
-
--- frontend/lib/presentation/pages/question_page.dart
+++ frontend/lib/presentation/pages/question_page.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:frontend/presentation/pages/judge_page.dart';
 
 class QuestionPage extends StatelessWidget {
-  const QuestionPage({Key? key}) : super(key: key);
+  String product_genre;
+  QuestionPage(this.product_genre);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-            child: Text("テスト"),
+            child: Text(this.product_genre),
       ),
     );
   }
